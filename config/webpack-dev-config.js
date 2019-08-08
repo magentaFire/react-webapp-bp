ec63c834ede211eddc2114c0f79b40ff2fb06abe
magentaFire <dafne.magenta@gmail.com>
2019-08-08 18:31:51 -0500

    Adds Linter and prettier

--- config/webpack-dev-config.js
+++ config/webpack-dev-config.js
@@ -1,4 +1,3 @@
-
 // webpack-dev-config.js
 
 // configuration data related to development only
@@ -11,52 +10,52 @@ const paths = require("./paths");
 const common = require("./webpack-common-config.js");
 
 module.exports = merge(common, {
-    entry: [paths.appIndexJs],
-    mode: "development",
-    // devtool option controls if and how source maps are generated.
-    // see https://webpack.js.org/configuration/devtool/
-    // If you find that you need more control of source map generation,
-    // see https://webpack.js.org/plugins/source-map-dev-tool-plugin/
-    devtool: "eval",
-    plugins: [
-        new webpack.HotModuleReplacementPlugin(),
-        new webpack.NamedModulesPlugin(),
-        new webpack.DefinePlugin({
-            "process.env": {
-                NODE_ENV: JSON.stringify("development")
+  entry: [paths.appIndexJs],
+  mode: "development",
+  // devtool option controls if and how source maps are generated.
+  // see https://webpack.js.org/configuration/devtool/
+  // If you find that you need more control of source map generation,
+  // see https://webpack.js.org/plugins/source-map-dev-tool-plugin/
+  devtool: "eval",
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin(),
+    new webpack.DefinePlugin({
+      "process.env": {
+        NODE_ENV: JSON.stringify("development")
+      }
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        include: path.resolve(paths.appSrc),
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/react"]
+          }
+        }
+      },
+      {
+        test: /\.(css|scss)$/,
+        include: [path.resolve(paths.appSrc)],
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 1,
+              // This enables local scoped CSS based in CSS Modules spec
+              modules: true
             }
-        })
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                include: path.resolve(paths.appSrc),
-                exclude: /(node_modules)/,
-                use: {
-                    loader: "babel-loader",
-                    options: {
-                        presets: ["@babel/react"]
-                    }
-                }
-            },
-            {
-                test: /\.(css|scss)$/,
-                include: [path.resolve(paths.appSrc)],
-                use: [
-                    {
-                        loader: "style-loader"
-                    },
-                    {
-                        loader: "css-loader",
-                        options: {
-                            importLoaders: 1,
-                            // This enables local scoped CSS based in CSS Modules spec
-                            modules: true,
-                        }
-                    }
-                ]
-            }
+          }
         ]
-    }
-})
+      }
+    ]
+  }
+});
