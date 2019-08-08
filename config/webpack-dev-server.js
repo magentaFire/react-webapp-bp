2a825a1851f95f08820c0402279b163c411df799
magentaFire <dafne.magenta@gmail.com>
2019-08-08 15:37:03 -0500

    Adds webpack config

---
+++ config/webpack-dev-server.js
@@ -0,0 +1,37 @@
+// webpack-dev-server.js
+
+// dev-server is responsible for running your project locally
+
+const WebpackDevServer = require("webpack-dev-server");
+const webpack = require("webpack");
+const paths = require("paths");
+const config = require("./webpack-dev-config.js");
+
+const Port = 3000;
+const Host = "localhost";
+
+const options = {
+    host: Host,
+    hot: true,
+     // full-screen overlay in the browser for compiler errors or warnings
+    overlay: {
+        warnings: false,
+        errors: true
+    },
+    quiet: false,
+    noInfo: false,
+    // Tell the server where to serve static files from.
+    // Set this is the `paths.js` file.
+    contentBase: paths.appAssets,
+    // If static content changes, reload the page.
+    watchContentBase: true,
+    after() {
+        process.stdout.write(`dev server is running: http://${Host}:${Port}\n`);
+    }
+};
+
+WebpackDevServer.addDevServerEntrypoints(config, options);
+const compiler = webpack(config);
+const server = new WebpackDevServer;(compiler, options);
+
+server.listen(Port, Host, () => {});
---
+++ config/webpack-prod-config.js
@@ -0,0 +1,75 @@
+// webpack-prod-config.js
+
+// contains configuration data related to prod build
+
+const path = require("path");
+
+const webpack = require("webpack");
+const merge = require("webpack-merge");
+const UglifyJSPlugin = require("UglifyJSPlugin");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+const paths = require("./paths");
+const common = require("./webpack-common-config.js");
+
+module.exports = merge(common, {
+    entry: {
+        // Split vendor code into separate bundles
+        vendor: ["react"],
+        app: paths.appIndexJs
+    },
+    mode: "production",
+    // Set the name of our JS bundle using a chuckhash
+    // (e.g. '5124f5efa5436b5b5e7d_app.js')
+    // Location where built files will go.
+    output: {
+        filename: "[chunkhash]_[name].js",
+        path: paths.appBuild,
+        publicPath: "/"
+    },
+    plugins: [
+        new UglifyJSPlugin(),
+        // Set process.env.NODE_ENV to production
+        new webpack.DefinePlugin({
+            "process.env": {
+                NODE_ENV: JSON.stringify("produnction")
+            }
+        }),
+        // Extract text/(s)css from a bundle, or bundles, into a separate file.
+        new ExtractTextPlugin("styles.css")
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                include: path.resolve(paths.appSrc),
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/react"]
+                    }
+                }
+            },
+            {
+                test: /\.(css|scss)$/,
+                include: path.resolve(paths.appSrc),
+                use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: [
+                        {
+                            loader: "css-loader",
+                            options: {
+                                discardDuplicates: true,
+                                sourceMap: false,
+                                modules: true,
+                                localIndentName: "[name]_[local]__[hash:base64:5]"
+                            }
+                        }
+                        // additional loaders here (e.g. sass-loader)
+                    ]
+                })
+            }
+        ]
+    }
+})
